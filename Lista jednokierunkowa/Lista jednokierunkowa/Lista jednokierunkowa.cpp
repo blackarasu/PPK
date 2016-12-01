d85bf992312ad333796a3704f0c675f478bc95bb
Bartek <Bartek@BLACKBLOOD-PC>
2016-12-01 12:37:31 +0100

    Dodanie odczytu wszystkich elementów z listy

--- Lista jednokierunkowa/Lista jednokierunkowa/Lista jednokierunkowa.cpp
+++ Lista jednokierunkowa/Lista jednokierunkowa/Lista jednokierunkowa.cpp
@@ -20,10 +20,19 @@ int main()
 	for (int i = 1;; i++) {
 		std::cout << "Type data " << i << " ";
 		std::cin >> data;
+		if (data == 0) {
+			break;
+		}
 		lista* newLista = new lista();
 		newLista->metadata = data;//wpisanie dany "data" do listy;
 		last->next = newLista;//dopisanie struktury na koniec całej listy
 		last = newLista;//przepisanie adresu astetniego elementu listy na pomocniczą ostatnią
+		
+	}
+	last = head->next;
+	while (last!=nullptr) {
+		std::cout << last->metadata << " ";
+		last = last->next;
 	}
     return 0;
 }
