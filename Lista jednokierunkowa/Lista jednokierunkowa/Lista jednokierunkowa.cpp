8bfe7ff5ec66acb9a099768a066482f8dfb1ac78
Bartek <Bartek@BLACKBLOOD-PC>
2016-12-01 12:17:17 +0100

    Dodanie: dopisywania do listy

--- Lista jednokierunkowa/Lista jednokierunkowa/Lista jednokierunkowa.cpp
+++ Lista jednokierunkowa/Lista jednokierunkowa/Lista jednokierunkowa.cpp
@@ -12,7 +12,18 @@ struct lista {
 
 int main()
 {
-
+	int data;
+	lista* head;
+	lista* last;
+	head = new lista();
+	last = head;
+	for (int i = 1;; i++) {
+		std::cout << "Type data " << i << " ";
+		std::cin >> data;
+		lista* newLista = new lista();
+		newLista->metadata = data;//wpisanie dany "data" do listy;
+		last->next = newLista;//dopisanie struktury na koniec ca³ej listy
+		last=newLista;//przepisanie adresu astetniego elementu listy na pomocnicz¹ ostatni¹
     return 0;
 }
 
